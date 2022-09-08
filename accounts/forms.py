9ca795d13e0c5539109ccc07c5d5042913e6a067
Monique <veldmanmonique@gmail.com>
2022-09-08 12:53:30 +0000

    Add personal account content and overall styling to several areas

---
+++ accounts/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from . import models
+
+
+class UserAccountForm(forms.ModelForm):
+
+    class Meta:
+        model = models.UserAccount
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+        }
+
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'form-shape'
+            self.fields[field].label = False
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserAccount
+from .forms import UserAccountForm
 
 
 def personal_account(request):
     """ Display the user's personal account. """
-    personal_account = get_object_or_404(UserAccount, user=request.user)
+    my_account = get_object_or_404(UserAccount, user=request.user)
+    form = UserAccountForm(instance=my_account)
+    order_history = my_account.orders.all()
+
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=my_account)
+        if form.is_valid():
+            form.save()
 
     template = 'accounts/account.html'
     context = {
-        'personal_account': personal_account,
+        'form': form,
+        'order_history': order_history,
     }
 
     return render(request, template, context)
