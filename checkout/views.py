be62393fe9a3130476993e7120fac4d04743b950
Monique <veldmanmonique@gmail.com>
2022-09-08 20:03:44 +0000

    Add checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.conf import settings
-from . import forms
 import os
-
+from django.shortcuts import render, redirect
+from django.conf import settings
 import stripe
+from . import forms
+from basket.contexts import basket_contents
 
-if os.path.isfile('env.py'):
-    import env
 
-stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
+
+
+# if os.path.isfile('env.py'):
+#     import env
+
+# stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
 
 # Create your views here.
 
@@ -17,11 +19,22 @@ stripe.api_key = os.environ.get('STRIPE_SECRET_KEY')
 def checkout(request):
     """ A view to render the page where personal details can be filled out """
 
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     print(cart)
     if not cart:
-        messages.error(request, 'basket is empty')
         return redirect('products')
+    
+    current_basket = basket_contents(request)
+    total = current_basket['total_cost']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     form = forms.CheckoutForm()
     template = 'checkout/checkout.html'
@@ -38,7 +51,7 @@ def checkout(request):
 
     context = {
         'form': form,
-        'S_P_K': os.environ.get('STRIPE_PUBLIC_KEY')
+        'S_P_K': stripe_public_key
         # 'client_secret': intent.client_secret,
         # 'intent': intent,
     }
