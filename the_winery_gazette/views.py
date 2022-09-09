a6cb0be153d40c94fc181fb607e579531eac6883
Monique <veldmanmonique@gmail.com>
2022-09-09 12:19:14 +0000

    Add detail view, url and template for gazette detail

--- the_winery_gazette/views.py
+++ the_winery_gazette/views.py
@@ -16,8 +16,6 @@ def the_gazette(request):
     return render(request, template_name, context)
 
 
-# class GazettePostDetail(generic.DetailView):
-#     """ A view to show the details of a specific post/article """
-
-#     model = Post
-#     template_name = 'the_winery_gazette/gazette_post_detail.html'
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'the_winery_gazette/gazette_post_detail.html'
