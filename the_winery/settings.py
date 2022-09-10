ef3beabfe829e6f8749f1ed1f012156f4be81bb2
Monique <veldmanmonique@gmail.com>
2022-09-10 10:31:11 +0000

    Add S3 Bucket for Static files

--- the_winery/settings.py
+++ the_winery/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     # Other
     'django_countries',
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -171,6 +172,25 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+if "USE_AWS" in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'the-winery'
+    AWS_S3_REGION_NAME = 'eu-west-3'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    #Override static and media URLS in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
+
 
 # Stripe
 STANDARD_DELIVERY = 12
