29a5e7a7f676e43d864332ad1bb2bcf9d11a6487
Monique <veldmanmonique@gmail.com>
2022-09-10 10:41:16 +0000

    Added cache control

--- the_winery/settings.py
+++ the_winery/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-winery.herokuapp.com', 'localhost']
 
@@ -174,6 +174,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if "USE_AWS" in os.environ:
+
+    #Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94600000'
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'the-winery'
     AWS_S3_REGION_NAME = 'eu-west-3'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
