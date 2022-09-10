3aa8fb82dda27a3dcacf61b757d26d415f31052b
Monique <veldmanmonique@gmail.com>
2022-09-10 06:34:24 +0000

    Add custom 404 page

--- the_winery/urls.py
+++ the_winery/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('the_winery_gazette/', include('the_winery_gazette.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'the_winery.views.handler404'
